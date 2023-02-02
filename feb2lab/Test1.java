6556e682af4bb1e4dee072af8094d5dbe8248ad7
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-02-02 18:01:47 +0530

    Add files via upload

---
+++ feb2lab/Test1.java
@@ -0,0 +1,16 @@
+package feb2lab;
+public class Test1 {
+	public static void main(String[] args) {
+int a=10;
+int b=20;
+int c=30;
+int d=40;
+int e=50;
+int avg;//Calulate avg
+avg=(a+b+c+d+e)/5;
+ System.out.println("Average of five numbers is: " + avg);
+		}}
+
+
+	
+	
---
+++ feb2lab/Test4.java
@@ -0,0 +1,14 @@
+package feb2lab;
+
+public class Test4 {
+	public static void main(String[] args) {
+		int a=125;
+		int b=24;
+		System.out.println("The sum is = " + (a+b));
+		System.out.println("The Substraction is = " + (a-b));
+		System.out.println("The Multiplication is = " + (a*b));
+		System.out.println("The Substraction is = " + (a/b));
+		
+	}
+
+}
