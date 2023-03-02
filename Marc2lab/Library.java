b82c853e3dd9578577eb1214fe444e5089924e59
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-02 17:59:22 +0530

    Add files via upload

---
+++ Marc2lab/Library.java
@@ -0,0 +1,27 @@
+package March2lab;
+public class Library { //create library class
+	//create instance variables
+	private int bookId;
+	private String bookName,bookAuthor;
+	// use getters setters method
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}}
+	
+
+
