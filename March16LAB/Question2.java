99eb9e793e9e4c2a5189f1a40b72f58e65a82b77
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-16 17:30:50 +0530

    Add files via upload

---
+++ March16LAB/Question2.java
@@ -0,0 +1,8 @@
+package March16LAB;
+// write a program to reverse every word in string 
+public class Question2 { //main class 
+				public static void main(String[] args) { // main method 
+				StringBuffer s=new StringBuffer("My Name is Saurav"); //take string buffer 
+				s.reverse();
+				System.out.println("After reversing :"+s); //print the output after reverse
+			}} 
